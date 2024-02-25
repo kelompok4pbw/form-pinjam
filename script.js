e6367a04ab40fa503474bd90bb438e73aa12c849
kelompok4pbw <160614818+kelompok4pbw@users.noreply.github.com>
2024-02-25 20:23:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Initialize Materialize components
-    M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), { coverTrigger: false});
     M.Sidenav.init(document.querySelectorAll('.sidenav'), {edge: 'right'});
     M.FormSelect.init(document.querySelectorAll('select'));
+
+    M.FormSelect.init(document.querySelectorAll('select'));
+    
+    var dropdown = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(dropdown, {
+        onCloseStart: function() {
+            var dropdownArrow = document.querySelector('.dropdown-arrow-trigger');
+            dropdownArrow.innerText = 'arrow_drop_down';
+        },
+        coverTrigger: false
+    });
+
+    dropdown.forEach(function(drop) {
+        drop.addEventListener('click', function() {
+            var dropdownArrow = document.querySelector('.dropdown-arrow-trigger');
+            var instance = M.Dropdown.getInstance(drop);
+            if (instance && instance.isOpen) {
+                dropdownArrow.innerText = 'arrow_drop_up';
+            }
+        });
+    });
 });
